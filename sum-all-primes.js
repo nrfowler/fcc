ef02401ec6b2a8223adbf510dedb474aaae3d2a2
nrfowler <nrfowler@gmail.com>
2017-03-20 16:02:59 -0500

    final solution

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -1,36 +1,33 @@
-// for i in potentialPrimes
-	// if isPrime(i,PrimeList)
-		// add to PrimeList
-		// add to Sum
-
-		
-		
-//isPrime(i, PrimeList){
-	
-	
+function isPrime(i, PrimeList){
+	var isPrime = true;
+	for ( var prime of PrimeList)
+	{
+		if (i % prime === 0)
+			isPrime = false;
+	}
+	return isPrime;
+}
 function sumPrimes(num) {
   if(num==2)
     return 2;
   else if (num<=2)
     return 0;
-  var prime = 2;
+  var potentialPrime = 2;
   var sum = 2;
   var listPrimes = [];
   listPrimes.push(2);
-  prime++;
-  var isPrime;
-  while(prime<=num){
-    if(listPrimes.reduce(
-      function(prev, el){
-        return (prime % el ===0||prev===false);}
-      ,true))
+  potentialPrime++;
+  while(potentialPrime<=num){
+    //noprotect
+    if(isPrime(potentialPrime,listPrimes))
     {
-      console.log(prime+' pushed');
-      listPrimes.push(prime);
-      sum+=prime;
+      console.log(potentialPrime+' pushed');
+      listPrimes.push(potentialPrime);
+      sum+=potentialPrime;
     }
-    console.log(prime);
-    prime++;
+    console.log(potentialPrime);
+    potentialPrime++;
   }
   return sum;
 }
+sumPrimes(977);
