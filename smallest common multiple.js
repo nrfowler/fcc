c820e5f7959fc18056021c335c973400a25a4c52
Nathan Fowler <nrfowler@users.noreply.github.com>
2017-05-22 12:44:05 -0700

    first pass at implementation

--- smallest common multiple.js
+++ smallest common multiple.js
@@ -5,16 +5,38 @@
 // e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
 
 function smallestCommons(arr) {
-  arr = arr.sort();
-  generate 10 multiples of each, find smallest match. If no match or is not divisible by range, generate 10 more.
-  return arr;
+  	arr = arr.sort();
+	var found = false;
+	var prevLCM=0, LCM=0;
+	while(!found){
+		LCM= generateNextLCM(prevLCM,min, max);
+		if(isDivByRange(LCM))
+			found=true;
+		else
+			prevLCM=LCM;
+	}
+	  return LCM;
 }
+
+function generateNextLCM(prevLCM,min, max){
+	var isNotLCM = true;
+	var possible = prevLCM+1;
+	while(isNotLCM){
+		if(possible % min == 0 && possible % max == 0 )
+			isNotLCM=false;
+		else
+			possible++;
+	}
+	return possible;
+}
+		
 function isDivByRange(div,arr){
   var isDiv = true;
-  for (var divider of arr){
-	if( div % divider != 0)
-		isDiv = false;
-}
+  for (var divider of arr)
+  	{
+		if( div % divider != 0)
+			isDiv = false;
+	}
 	
   return isDiv;
 }
